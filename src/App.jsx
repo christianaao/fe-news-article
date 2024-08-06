317133d5b2c29e6561b386ef6c3c2a0555875ca1
Olamide Olawuyi <christianaao@ChristianaO-Laptop>
2024-08-06 15:35:46 +0100

    completed article cards plus some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import "./styling/App.css"
 import { Header } from './components/Header'
+import { ArticleCards } from './components/ArticleCards'
 
 function App() {
     return (
         <div>
             <Header/>
-
+            <ArticleCards/>
         </div>
     )
 }
--- src/components/api.js
+++ src/components/api.js
@@ -3,3 +3,14 @@ import axios from "axios"
 const api = axios.create({
     baseURL: "https://news-article-project.onrender.com/api/"
 })
+
+const getArticles = () => {
+    return api.get("/articles")
+    .then(({data}) => {
+        console.log(data)
+        console.dir(data)
+        return data
+    })
+}
+
+export { getArticles }
